31522046d9fc13d42be713c63997e15c5221a43e
Jonny Zhang <jonathan_zhang@college.harvard.edu>
2020-04-23 08:00:06 -0700

    test

--- lab20.ml
+++ lab20.ml
@@ -1,26 +1,26 @@
 type image = float list list ;;
 (* images are lists of lists of floats between 0. (white) and 1. (black) *)
 type size = int * int ;;
-open Graphics ;;
+
   
 (* threshold thershold image -- image where pixels above the threshold
 value are black *)
-let threshold (img : float list list) (threshold : float) : float list list =
+let threshold (img : image) (threshold : float) : float list list =
   List.map  (fun row -> List.map (fun v -> if v <= threshold then 0. else 1.)
                                  row) img
        
 (* show the image *)
-let depict (img : float list list) : unit =
+let depict (img : image) : unit =
   Graphics.open_graph ""; Graphics.clear_graph ();
   let x, y = List.length (List.hd img), List.length img in Graphics.resize_window x y;
   let depict_pix v r c = let lvl = int_of_float (255. *. (1. -. v)) in Graphics.set_color (Graphics.rgb lvl lvl lvl);
-  plot c (y - r) in
+  Graphics.plot c (y - r) in
   List.iteri (fun r row -> List.iteri (fun c pix -> depict_pix pix r c) row) img;
   Unix.sleep 2; Graphics.close_graph () ;;
 
 (* dither max image -- dithered image *)
 
-let dither (img : float list list) =
+let dither (img : image) =
   List.map
     (fun row ->
      List.map
